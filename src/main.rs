b4914d6f2df11337441a79ab43b6317df99c6300
Husseljo <husseljoo@gmail.com>
2021-07-25 02:51:40 +0200

    handle negative inputs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,23 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
+struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value<1 || value>100 {
+            panic!("{} is out of range",value);
+        }
+        Guess{value}
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -15,25 +32,13 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        //println!("You guessed {}",guess);
-        //let guess: u32 = guess.trim().parse().expect("Please type a number!");
-        
-        let guess: u32 = match guess.trim().parse(){
-            Ok(num)=>num,
-            Err(_)=>{
-                println!("Not a valid number my lord!");
-                continue;
-            }
-            };
+        let num= Guess::new(guess.trim().parse().unwrap());
 
-        match guess.cmp(&secret_number){
+        match Guess::value(&num).cmp(&secret_number){
             Ordering::Less=> println!("Too small!"),
             Ordering::Greater=> println!("Too big!"),
             Ordering::Equal=> {
-                println!("You win!");
-                break;
-                }
-            }
+                println!("You win!"); break; } }
     }
 
 }
